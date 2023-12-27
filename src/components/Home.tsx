875709cd562f26d0fd91479fd70bc396c67fbf27
Ryan <mckevitt.r@gmail.com>
2023-12-26 23:55:44 -0600

    home parses fetched data to render all toll objects for logged in user

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { app } from '../scripts/firebaseConfig';
+import { getDatabase, ref, child, onValue, DataSnapshot } from "firebase/database";
 
 const Home: React.FC = () => {
+    const [displayable, setDisplayable] = useState<string | null>(null);
+    const db = getDatabase(app);
+    const dbRef = ref(db);
+
+    useEffect(() => {
+        const fetchData = () => {
+            const unsubscribe = onValue(child(dbRef, 'users'), (snapshot: DataSnapshot) => {
+                if (snapshot.exists()) {
+                    const currentUserTollsList = snapshot.val()["8cdV1LLiTbZtNnQCoRYvs5qBhSn2"]["tolls"];
+                    let displayVal: string = ""
+                    currentUserTollsList.forEach((_: JSON, index: number) => {
+                        const gateId: string = JSON.stringify(currentUserTollsList[index]["gateId"]);
+                        const year: number = Number(currentUserTollsList[index]["timestamp"]["year"]) + 1900;
+                        const month: number = Number(currentUserTollsList[index]["timestamp"]["month"]) + 1;
+                        const date: string = JSON.stringify(currentUserTollsList[index]["timestamp"]["date"]);
+                        const dateFull: string = `${year} ${month} ${date}`
+                        displayVal += (`${dateFull}: ${gateId}`);
+                    })
+                    setDisplayable(displayVal);
+                } else {
+                    setDisplayable("No data available");
+                }
+            });
+            return () => unsubscribe();
+        };
+
+        fetchData();
+    }, [dbRef]);
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <p>{displayable}</p>
         </div>
     );
 }

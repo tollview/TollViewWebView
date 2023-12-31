86280b567cdbb0888bac981603d13810e7803bc0
Ryan <mckevitt.r@gmail.com>
2023-12-31 01:55:38 -0600

    slightly formatted DayReports

--- src/components/DayReport.tsx
+++ src/components/DayReport.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const DayReport = ({thisDayAndCost}) => {
+interface DayReportProps {
+    thisDayAndCost: [number, number]; // Assuming both elements are numbers
+}
+
+const DayReport: React.FC<DayReportProps> = ({ thisDayAndCost }) => {
+    const formatCurrency = (value: number) => {
+        return value.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+        });
+    };
+
     return (
         //todo don't worry this will be proper css eventually - just blasting thru some test displays
         <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "75vw" }}>
-            <h2>{JSON.stringify(thisDayAndCost[0])}</h2>
-            <h2>{JSON.stringify(thisDayAndCost[1])}</h2>
+            <h2>{thisDayAndCost[0]}</h2>
+            <h2>{formatCurrency(thisDayAndCost[1])}</h2>
         </div>
     )
 }
 
 export default DayReport
+
